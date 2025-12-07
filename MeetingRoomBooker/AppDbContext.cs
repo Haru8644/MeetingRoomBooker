cad6cc529fc10749ae6fa3951511bec6321fdbfc
Haru8644 <haruki_sasuke@icloud.com>
2025-12-08 05:03:47 +0900

    feat: integrate SQLite database with EF Core, implement delete button and duplicate check

---
+++ MeetingRoomBooker/AppDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MeetingRoomBooker.Components.Pages;
+
+namespace MeetingRoomBooker
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options): base(options)
+        { 
+            Database.EnsureCreated();
+        }
+        public DbSet<Reservation.ReservationModel> Reservations { get; set; }
+    }
+}
