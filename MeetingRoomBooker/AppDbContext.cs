9937ea70772b72a3fde19ead0e2755f3ca193862
Haru8644 <haruki_sasuke@icloud.com>
2025-12-15 06:35:52 +0900

    Complete prototype: Updated UI and Mock data

--- MeetingRoomBooker/AppDbContext.cs
+++ MeetingRoomBooker/AppDbContext.cs
@@ -1,15 +1,8 @@
 ﻿using Microsoft.EntityFrameworkCore;
-using MeetingRoomBooker.Components.Pages;
 using MeetingRoomBooker.Models;
 
-namespace MeetingRoomBooker
+public class AppDbContext : DbContext
 {
-    public class AppDbContext : DbContext
-    {
-        public AppDbContext(DbContextOptions<AppDbContext> options): base(options)
-        { 
-            Database.EnsureCreated();
-        }
-        public DbSet<ReservationModel> Reservations { get; set; }
-    }
+    public AppDbContext(DbContextOptions<AppDbContext> options): base(options){}
+    public DbSet<ReservationModel> Reservations { get; set; } = default!;
 }
--- MeetingRoomBooker/Services/DbBookingService.cs
+++ MeetingRoomBooker/Services/DbBookingService.cs
@@ -11,25 +11,21 @@ namespace MeetingRoomBooker.Services
         {
             _context = context;
         }
-
         public async Task<List<ReservationModel>> GetReservationsAsync()
         {
             await _context.Database.EnsureCreatedAsync();
             return await _context.Reservations.ToListAsync();
         }
-
         public async Task AddReservationAsync(ReservationModel reservation)
         {
             _context.Reservations.Add(reservation);
             await _context.SaveChangesAsync();
         }
-
         public async Task RemoveReservationAsync(ReservationModel reservation)
         {
             _context.Reservations.Remove(reservation);
             await _context.SaveChangesAsync();
         }
-
         public async Task UpdateReservationAsync(ReservationModel reservation)
         {
             var target = await _context.Reservations.FindAsync(reservation.Id);
@@ -37,9 +33,12 @@ namespace MeetingRoomBooker.Services
             {
                 target.Name = reservation.Name;
                 target.Room = reservation.Room;
+                target.NumberOfPeople = reservation.NumberOfPeople; 
                 target.Date = reservation.Date;
                 target.StartTime = reservation.StartTime;
                 target.EndTime = reservation.EndTime;
+                target.Type = reservation.Type;
+                target.Purpose = reservation.Purpose;
                 await _context.SaveChangesAsync();
             }
         }
