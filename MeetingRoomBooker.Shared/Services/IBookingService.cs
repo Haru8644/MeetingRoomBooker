8245ffa5da7068d7dcceda1795dd281ffac2b663
Haru8644 <219149071+Haru8644@users.noreply.github.com>
2026-03-01 22:34:59 +0900

    hotfix: restore build by repairing shared models and web services

--- MeetingRoomBooker.Shared/Services/IBookingService.cs
+++ MeetingRoomBooker.Shared/Services/IBookingService.cs
@@ -8,14 +8,6 @@ namespace MeetingRoomBooker.Shared.Services
     public interface IBookingService
     {
         Task<UserModel?> LoginAsync(string email, string password);
-using System.Collections.Generic;
-using System.Threading.Tasks;
-
-namespace MeetingRoomBooker.Services
-{
-    public interface IBookingService
-    {
-        Task<UserModel> LoginAsync(string email, string password);
         Task<bool> RegisterUserAsync(UserModel user);
         Task DeleteUserAsync(int userId);
 
@@ -26,7 +18,9 @@ namespace MeetingRoomBooker.Services
         Task AddReservationAsync(ReservationModel reservation);
         Task RemoveReservationAsync(ReservationModel reservation);
         Task UpdateReservationAsync(ReservationModel reservation, bool shouldNotify);
+
         Task<List<UserModel>> GetAllUsersAsync();
+
         Task<List<NotificationModel>> GetNotificationsAsync(int userId);
         Task AddNotificationAsync(NotificationModel notification);
         Task DeleteNotificationAsync(int notificationId);
