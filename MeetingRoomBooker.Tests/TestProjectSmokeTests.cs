027b2d4ea60b618541f0ca389c7248c2de52a2dc
Haru8644 <219149017+Haru8644@users.noreply.github.com>
2026-05-10 03:43:38 +0900

    test: add xUnit test project

---
+++ MeetingRoomBooker.Tests/TestProjectSmokeTests.cs
@@ -0,0 +1,10 @@
+namespace MeetingRoomBooker.Tests;
+
+public class TestProjectSmokeTests
+{
+    [Fact]
+    public void TestProject_can_run_xunit_tests()
+    {
+        Assert.True(true);
+    }
+}
